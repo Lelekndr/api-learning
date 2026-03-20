d6c77e30b3b050adc580f8d755e38658939ecea6
Leandro Mendieta <leandro.mendieta13@gmail.com>
2026-03-20 15:03:16 -0400

    Setup schemas

---
+++ prisma/migrations/20260320142702_add_book_category/migration.sql
@@ -0,0 +1,37 @@
+-- DropForeignKey
+ALTER TABLE "Book" DROP CONSTRAINT "Book_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "pages" DROP NOT NULL,
+ALTER COLUMN "status" SET DEFAULT 'quero_ler';
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_BookToCategory" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_BookToCategory_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE INDEX "_BookToCategory_B_index" ON "_BookToCategory"("B");
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BookToCategory" ADD CONSTRAINT "_BookToCategory_A_fkey" FOREIGN KEY ("A") REFERENCES "Book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_BookToCategory" ADD CONSTRAINT "_BookToCategory_B_fkey" FOREIGN KEY ("B") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
